131d6dd1a51b0eed2d03c163ba17a13290dd2008
Ravoxsg <mathieu.ravaut@student.ecp.fr>
2018-03-13 17:47:02 -0400

    refactored version of the code, mostly of main.py

--- loss.py
+++ loss.py
@@ -4,6 +4,9 @@ import torch
 import torch.nn as nn
 from torch.autograd import Variable
 
+import utils
+
+
 class NLLLoss(nn.Module):
     """Self-Defined NLLLoss Function
 
@@ -39,5 +42,76 @@ class NLLLoss(nn.Module):
         loss = torch.masked_select(prob, one_hot)
         return -torch.sum(loss)
 
+
+class GANLoss(nn.Module):
+    """Reward-Refined NLLLoss Function for adversial training of Gnerator"""
+    def __init__(self):
+        super(GANLoss, self).__init__()
+
+    def forward_reinforce(self, prob, target, reward, cuda=False):
+        """
+        Args:
+            prob: (N, C), torch Variable 
+            target : (N, ), torch Variable
+            reward : (N, ), torch Variable
+        """
+        N = target.size(0)
+        C = prob.size(1)
+        one_hot = torch.zeros((N, C))
+        if cuda:
+            one_hot = one_hot.cuda()
+        one_hot.scatter_(1, target.data.view((-1,1)), 1)
+        one_hot = one_hot.type(torch.ByteTensor)
+        one_hot = Variable(one_hot)
+        if cuda:
+            one_hot = one_hot.cuda()
+        loss = torch.masked_select(prob, one_hot)
+        loss = loss * reward
+        loss =  -torch.sum(loss)
+        return loss
+    
+    def old_forward(self, prob, target, reward, c_phi_hat, discriminator, BATCH_SIZE, g_sequence_len, cuda=False):
+        """
+        Forward function with implementation based on the original one.
+        """
+        N = target.size(0)
+        C = prob.size(1)
+        one_hot = torch.zeros((N, C))
+        if cuda:
+            one_hot = one_hot.cuda()
+        one_hot.scatter_(1, target.data.view((-1,1)), 1)
+        one_hot = one_hot.type(torch.ByteTensor)
+        one_hot = Variable(one_hot)
+        if cuda:
+            one_hot = one_hot.cuda()
+        loss = torch.masked_select(prob, one_hot)
+        loss = loss.view(BATCH_SIZE, g_sequence_len)
+        loss = torch.mean(loss, 1)
+        c_phi_z, c_phi_z_tilde = utils.c_phi_out(c_phi_hat, prob, discriminator, cuda)
+        c_phi_z_tilde = c_phi_z_tilde[:,1]
+        c_phi_z = c_phi_z[:,1]
+        loss = loss * (reward - c_phi_z_tilde) + c_phi_z - c_phi_z_tilde
+        loss =  - torch.sum(loss)
+        return loss
+
+    def forward(self, prob, samples, reward, c_phi_hat, discriminator, BATCH_SIZE, g_sequence_len, VOCAB_SIZE, cuda=False):
+        """
+        Computes the Generator's loss in RELAX optimization setting. 
+
+        """
+        prob_temp = prob.view(BATCH_SIZE, g_sequence_len, VOCAB_SIZE)
+        new_prob = Variable(torch.zeros(BATCH_SIZE, g_sequence_len))
+        if cuda:
+            new_prob = new_prob.cuda()
+        for i in range(BATCH_SIZE):
+            for j in range(g_sequence_len):
+                new_prob[i,j] = prob_temp[i,j,int(samples[i,j])]
+        loss = torch.sum(new_prob, 1)
+        c_phi_z, c_phi_z_tilde = utils.c_phi_out(c_phi_hat, prob, discriminator, cuda)
+        c_phi_z_tilde = c_phi_z_tilde[:,1]
+        c_phi_z = c_phi_z[:,1]
+        loss = loss * (reward - c_phi_z_tilde) + c_phi_z - c_phi_z_tilde
+        loss =  - torch.sum(loss)
+        return loss
 
 
---
+++ utils.py
@@ -0,0 +1,131 @@
+'''
+Auxiliary functions used during training.
+'''
+
+import os
+import random
+import math
+
+import argparse
+import tqdm
+
+import numpy as np
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.autograd import Variable
+
+from generator import Generator
+from discriminator import Discriminator
+from annex_network import AnnexNetwork
+from target_lstm import TargetLSTM
+from rollout import Rollout
+from data_iter import GenDataIter, DisDataIter
+
+from main import g_sequence_len, BATCH_SIZE, VOCAB_SIZE
+
+
+def generate_samples(model, batch_size, generated_num, output_file):
+    samples = []
+    for _ in range(int(generated_num / batch_size)):
+        sample = model.sample(batch_size, g_sequence_len).cpu().data.numpy().tolist()
+        samples.extend(sample)
+    with open(output_file, 'w') as fout:
+        for sample in samples:
+            string = ' '.join([str(s) for s in sample])
+            fout.write('%s\n' % string)
+
+def train_epoch(model, data_iter, criterion, optimizer, cuda=False):
+    total_loss = 0.
+    total_words = 0.
+    for (data, target) in data_iter:
+    	#tqdm(#data_iter, mininterval=2, desc=' - Training', leave=False):
+        data = Variable(data)
+        target = Variable(target)
+        if cuda:
+            data, target = data.cuda(), target.cuda()
+        target = target.contiguous().view(-1)
+        pred = model.forward(data)
+        loss = criterion(pred, target)
+        total_loss += loss.data[0]
+        total_words += data.size(0) * data.size(1)
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+    data_iter.reset()
+    return math.exp(total_loss / total_words)
+
+def eval_epoch(model, data_iter, criterion, cuda=False):
+    total_loss = 0.
+    total_words = 0.
+    for (data, target) in data_iter:
+    	#tqdm(#data_iter, mininterval=2, desc=' - Training', leave=False):
+        data = Variable(data, volatile=True)
+        target = Variable(target, volatile=True)
+        if cuda:
+            data, target = data.cuda(), target.cuda()
+        target = target.contiguous().view(-1)
+        pred = model.forward(data)
+        loss = criterion(pred, target)
+        total_loss += loss.data[0]
+        total_words += data.size(0) * data.size(1)
+    data_iter.reset()
+    return math.exp(total_loss / total_words)
+
+# New functions/classes
+
+# return probability distribution (in [0,1]) at the output of G
+def g_output_prob(prob):
+    softmax = nn.Softmax(dim=0)
+    theta_prime = softmax(prob)
+    return theta_prime
+
+# performs a Gumbel-Softmax reparameterization of the input
+def gumbel_softmax(theta_prime, VOCAB_SIZE, cuda=False):
+    u = Variable(torch.log(-torch.log(torch.rand(VOCAB_SIZE))))
+    if cuda:
+        u = Variable(torch.log(-torch.log(torch.rand(VOCAB_SIZE)))).cuda()
+        theta_prime = theta_prime.cuda()
+    z = torch.log(theta_prime) - u
+    return z
+
+# categorical re-sampling exactly as in Backpropagating through the void - Appendix B
+def categorical_re_param(theta_prime, VOCAB_SIZE, b, cuda=False):
+    v = Variable(torch.rand(theta_prime.size(0), VOCAB_SIZE))
+    if cuda:
+        v = v.cuda()
+    z_tilde = -torch.log(-torch.log(v)/theta_prime - torch.log(v[:,b]))
+    z_tilde[:,b] = -torch.log(-torch.log(v[:,b]))
+    return z_tilde
+
+# when you have sequences as probability distributions, re-puts them into sequences by doing argmax
+def prob_to_seq(x, cuda=False):
+    x_refactor = Variable(torch.zeros(x.size(0), x.size(1)))
+    if cuda:
+        x_refactor = x_refactor.cuda()
+    for i in range(x.size(1)):
+        x_refactor[:,i] = torch.max(x[:,i,:], 1)[1]
+    return x_refactor
+
+#3 e and f : Defining c_phi and getting c_phi(z) and c_phi(z_tilde)
+def c_phi_out(c_phi_hat, theta_prime, discriminator, cuda=False):
+    z = gumbel_softmax(theta_prime, VOCAB_SIZE, cuda)
+    value, b = torch.max(z,0)
+    z_tilde = categorical_re_param(theta_prime, VOCAB_SIZE, b, cuda)
+    z_gs = gumbel_softmax(z, VOCAB_SIZE, cuda)
+    z_tilde_gs = gumbel_softmax(z_tilde, VOCAB_SIZE, cuda)
+    # reshaping the inputs of the discriminator
+    z_gs = z_gs.view(BATCH_SIZE, g_sequence_len, VOCAB_SIZE)
+    z_gs = prob_to_seq(z_gs, cuda)
+    z_gs = z_gs.type(torch.LongTensor)
+    if cuda:
+        z_gs = z_gs.cuda()
+    z_tilde_gs = z_tilde_gs.view(BATCH_SIZE, g_sequence_len, VOCAB_SIZE)
+    z_tilde_gs = prob_to_seq(z_tilde_gs, cuda)
+    z_tilde_gs = z_tilde_gs.type(torch.LongTensor)
+    if cuda:
+        z_tilde_gs = z_tilde_gs.cuda()
+    
+    #return c_phi_hat.forward(z),c_phi_hat.forward(z_tilde)
+    return c_phi_hat.forward(z)+discriminator.forward(z_gs),c_phi_hat.forward(z_tilde)+discriminator.forward(z_tilde_gs)
