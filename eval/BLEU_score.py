3dfc3effb4922b355559521225157ebaa250d59e
Raeid Saqur <raeidsaqur@cs.toronto.edu>
2018-03-13 06:43:23 -0400

    Added BLEU_score calculator

--- eval/BLEU_score.py
+++ eval/BLEU_score.py
@@ -80,15 +80,11 @@ def __get_precision(candidate, references, n):
 
 def BLEU_score(candidate, references, n):
     """
-	Compute the LOG probability of a sentence, given a language model and whether or not to
-	apply add-delta smoothing
-	
 	INPUTS:
 	sentence :	(string) Candidate sentence.  "SENTSTART i am hungry SENTEND"
 	references:	(list) List containing reference sentences. ["SENTSTART je suis faim SENTEND", "SENTSTART nous sommes faime SENTEND"]
 	n :			(int) one of 1,2,3. N-Gram level.
 
-	
 	OUTPUT:
 	bleu_score :	(float) The BLEU score
 	"""
