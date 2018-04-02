fixed the way to sum gradients squared

loss.py
@@ -161,16 +161,14 @@ class VarianceLoss(nn.Module):
 
     def forward(self, grad, cuda = False):
         bs = len(grad)
-        total_loss = Variable(torch.zeros(1), requires_grad=True)
+        ref = 0
+        for j in range(bs):
+            for i in range(len(grad[j])):
+                ref += torch.sum(grad[j][i]**2).item()
+        total_loss = np.array([ref/bs])
+        total_loss = Variable(torch.Tensor(total_loss), requires_grad=True)
+        print(total_loss)
         if cuda:
             total_loss = total_loss.cuda()
-        for j in range(bs):
-            batch_j_loss = Variable(torch.zeros(1), requires_grad=True)
-            ref = 0
-            if cuda:
-                batch_j_loss = total_loss.cuda()
-            for i in range(len(grad[j])):
-                batch_j_loss = torch.add(batch_j_loss, torch.sum(grad[j][i]**2))
-            total_loss = torch.add(total_loss, batch_j_loss)
-        return total_loss/bs
+        return total_loss
 

main.py
@@ -33,7 +33,7 @@ THC_CACHING_ALLOCATOR = 0
 SEED = 88
 random.seed(SEED)
 np.random.seed(SEED)
-BATCH_SIZE = 16
+BATCH_SIZE = 64
 GENERATED_NUM = 10000
 # related to data
 POSITIVE_FILE = 'data/math_equation_data.txt'