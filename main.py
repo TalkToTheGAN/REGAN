661199b24ed25e7009ec02d904d3082112c8bf8f
Raeid Saqur <raeidsaqur@cs.toronto.edu>
2018-04-07 11:32:57 -0400

    Added experiment results.

--- main.py
+++ main.py
@@ -53,7 +53,7 @@ PRE_EPOCH_GEN = 2 if isDebug else 120 # can be a decimal number
 PRE_EPOCH_DIS = 0 if isDebug else 5
 PRE_ITER_DIS = 0 if isDebug else 3
 # adversarial training
-GD = "REINFORCE" # "REINFORCE" or "REBAR" or "RELAX"
+GD = "RELAX" # "REINFORCE" or "REBAR" or "RELAX"
 CHECK_VARIANCE = False
 if GD == "RELAX":
     CHECK_VARIANCE = True
@@ -78,7 +78,7 @@ d_dropout = 0.75
 d_num_class = 2
 d_lstm_hidden_dim = 32
 DEFAULT_ETA = 1             #for REBAR only. Note: Naive value, in paper they estimate value
-DEFAULT_TEMPERATURE = 0.9
+DEFAULT_TEMPERATURE = 1
 # Annex network parameters
 c_filter_sizes = [1, 3, 5, 7, 9, 15]
 c_num_filters = [100, 200, 200, 200, 100, 100]
